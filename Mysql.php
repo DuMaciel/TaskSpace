396d81cd60a07e3fa152c3f8e85093e98c084a58
DuMaciel <edumaciel1221@gmail.com>
2022-12-13 08:47:31 -0300

    refect: Mysql has been turned into static class

--- Mysql.php
+++ Mysql.php
@@ -2,36 +2,35 @@
 include 'Dbconfig.php';
 
 class Mysql{
-    public mysqli $mysql;
+    static public mysqli $mysql;
 
     function __construct(){
-        $dbconfig = new Dbconfig();
-        $this->mysql = $dbconfig->getConnection();
+        self::$mysql = Dbconfig::getConnection();
     }
     
-    function insertUser($name, $email, $pass){
-        $name = $this->mysql->real_escape_string($name);
-        $email = $this->mysql->real_escape_string($email);
+    static function insertUser($name, $email, $pass){
+        $name = self::$mysql->real_escape_string($name);
+        $email = self::$mysql->real_escape_string($email);
         $pass = password_hash($pass, PASSWORD_DEFAULT);
 
-        if(!$this->userExist($email)){
-            $resultado = $this->mysql->query("INSERT INTO users (_name, _email, _pass) VALUES ('$name','$email','$pass');");
+        if(!self::userExist($email)){
+            $resultado = self::$mysql->query("INSERT INTO users (_name, _email, _pass) VALUES ('$name','$email','$pass');");
             return $resultado;
         }
         return false;
     }
 
-    function testPass($email, $pass){
-        if($this->userExist($email)){
-            $passHash = $this->mysql->query("SELECT _pass FROM users WHERE _email = '$email'")->fetch_assoc()['_pass'];
+    static function testPass($email, $pass){
+        if(self::userExist($email)){
+            $passHash = self::$mysql->query("SELECT _pass FROM users WHERE _email = '$email'")->fetch_assoc()['_pass'];
             $resultado = password_verify($pass, $passHash);
             return $resultado;
         }
         return false;
     }
 
-    function userExist($email){
-        $resultado = $this->mysql->query("SELECT _email FROM users WHERE _email = '$email'");
+    static function userExist($email){
+        $resultado = self::$mysql->query("SELECT _email FROM users WHERE _email = '$email'");
         return ($resultado->num_rows > 0);
     }
 
