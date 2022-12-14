cc95bac7f20f215d09c4abe81b09a1e288c316ab
DuMaciel <edumaciel1221@gmail.com>
2022-12-14 14:41:12 -0300

    refect: register separation between module and template

---
+++ src/modules/register.php
@@ -0,0 +1,24 @@
+<?php
+
+    if(isset($_POST['email'])){
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        
+        include realpath(__DIR__ . '/../database/Mysql.php');
+        $db = new Mysql();
+
+        if(!$db::userExist($email)){
+            if($db::insertUser($name, $email, $password)){
+                echo 'Usuario Cadastrado';
+            }else{
+                echo 'Erro ao cadastrar';
+            }
+        }else{
+            echo 'Usuario ja existe';
+        }
+        
+    }
+   
+?>
+
