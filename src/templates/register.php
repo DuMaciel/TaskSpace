ec2b8de67d79abb024645e266c438b19358ba70d
DuMaciel <edumaciel1221@gmail.com>
2022-12-15 15:56:27 -0300

    refect: HTML 'register.php'

--- src/templates/register.php
+++ src/templates/register.php
@@ -3,29 +3,45 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="./public/styles/normalize.css">
     <link rel="stylesheet" href="./public/styles/register.css">
     <title>Registration ToDoList</title>
 </head>
 <body>
-    <form  action="./register" method="post" id='registerForm'>
-        <fieldset>
-            <legend>Registration</legend>
-            <label for="name">Name</label>
-            <input type="name" id="name" name="name" required autofocus>
-            <label for="email">Email</label>
-            <input type="email" id="email" name="email" required>
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" required>
-            <label for="conf-password">Confirm password</label>
-            <input type="password" id="confPassword" required>
-            <input type="submit" value="Submit" id="submit">
-        </fieldset>
-        <a href="./login">I'm already a user</a>
+    <header>
+        <a href="">TaskSpace</a>
+    </header>
+    <section class="register">
+    <form  action="./register" method="post">
+            <p class="title">Hello!</p>
+            <p class="caption">Welcome To TaskSpace<br>sign up to get started.</p>
+            <label class="hidden" for="name">Name</label>
+            <input type="text" id="name" name="name" placeholder="Enter your name" required autofocus>
+            <label class="hidden" for="email">Email</label>
+            <input type="email" id="email" name="email" placeholder="Enter your email" required>
+            <label class="hidden" for="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="Enter your password" required>
+            <label class="hidden" for="conf-password">Confirm password</label>
+            <input type="password" id="confPassword" placeholder="Confirm your password" required>
+            <input class="button" type="submit" value="SIGN UP" id="SingUp">
+            <span class="reminder">Already have an account? <a class="link" href="./login">Log In</a></span>
     </form>
+    </section>
     
+    <footer>
+        <p>TaskSpace®</p>
+    </footer>
 </body>
 
 <script src="./public/scripts/register.js"></script>
 </html>
+
+<?php ?>
