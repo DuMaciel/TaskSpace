96adcab1db7d463ba15bb85c58d9deee68265e54
DuMaciel <edumaciel1221@gmail.com>
2022-12-16 08:49:57 -0300

    refect: HTML 'error.php'

--- src/templates/error.php
+++ src/templates/error.php
@@ -1,12 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="./public/styles/normalize.css">
+    <link rel="stylesheet" href="./public/styles/error.css">
     <title>Page Not Found</title>
 </head>
+
 <body>
-    <h1>Page Not Found</h1>
+    <header>
+        <a href="./home">TaskSpace</a>
+    </header>
+
+    <main class="error">
+        <p class="art">404</p>
+        <h2 class="title">The page you are looking for cannot be found.</h2>
+        <p class="reminder">Go to <a class="link" href="./home">Home</a></p>
+    </main>
+
+
+    <footer>
+        <p>TaskSpace®</p>
+    </footer>
 </body>
+
 </html>
+
+<?php ?>
