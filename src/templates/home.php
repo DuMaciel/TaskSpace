75ae33370d67e5e563ef6961c49ccf43c3bf8041
DuMaciel <edumaciel1221@gmail.com>
2022-12-16 09:56:48 -0300

    feat: HTML 'home.php'

---
+++ src/templates/home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="./public/styles/normalize.css">
+    <link rel="stylesheet" href="./public/styles/home.css">
+    <title>To Do List | Organize seu dia!</title>
+</head>
+<body>
+    <header>
+        <a href="./home">TaskSpace</a>
+    </header>
+
+    <main class="home">
+        <img src="./public/images/taskMenager.svg" alt="">
+
+        <p class="title">Manage your tasks</p>
+        <p class="caption">Organize, plan, and collaborate on tasks with TaskSpace.Your busy life deserves this.you can manage checklist and your goal. </p>
+
+        <a class="button" href="./register">Get Started</a>
+        <p class="reminder">Already have an account? <a class="link" href="./login">Log In</a></p>
+    </main>
+    
+    <footer>
+        <p>TaskSpace®</p>
+    </footer>
+</body>
+</html>
