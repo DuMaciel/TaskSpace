1e8cc68659ae726a623927e9dd714d16de73c7e0
dumaciel <edumaciel1221@gmail.com>
2022-12-30 16:05:04 -0300

    refect: boards structures

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -3,31 +3,33 @@ const boards = [
     {
         id: 1,
         title: 'Meu Board',
+        columnsCount: 2,
         taskCount: 7,
         columns: [{
             id: 1,
             title: "Coluna 1",
-            tasks:  [{id: 1, title: 'Minha task 1', descripition: 'minha task bla bla'}, {id: 2, title: 'Minha task 2', descripition: 'minha task bla bla'},{id: 3, title: 'Minha task 3', descripition: 'minha task bla bla'}, {id: 4, title: 'Minha task 4', descripition: 'minha task bla bla'}]
+            tasks:  [{id: 1, title: 'Minha task 1', description: 'minha task bla bla'}, {id: 2, title: 'Minha task 2', description: 'minha task bla bla'},{id: 3, title: 'Minha task 3', description: 'minha task bla bla'}, {id: 4, title: 'Minha task 4', description: 'minha task bla bla'}]
         },
         {
             id: 2,
             title: "Coluna 2",
-            tasks: [{id: 5, title: 'Minha task 5', descripition: 'minha task bla bla'}, {id: 6, title: 'Minha task 6', descripition: 'minha task bla bla'}, {id: 7, title: 'Minha task 7', descripition: 'minha task bla bla'}]
+            tasks: [{id: 5, title: 'Minha task 5', description: 'minha task bla bla'}, {id: 6, title: 'Minha task 6', description: 'minha task bla bla'}, {id: 7, title: 'Minha task 7', description: 'minha task bla bla'}]
         }],
     },
     {
         id: 2,
         title: 'Meu Board 2',
+        columnsCount: 2,
         taskCount: 7,
         columns: [{
             id: 1,
             title: "Coluna 1",
-            tasks:  [{id: 1, title: 'Minha task 1', descripition: 'minha task bla bla'}, {id: 2, title: 'Minha task 2', descripition: 'minha task bla bla'},{id: 3, title: 'Minha task 3', descripition: 'minha task bla bla'}, {id: 4, title: 'Minha task 4', descripition: 'minha task bla bla'}]
+            tasks:  [{id: 1, title: 'Minha task 1', description: 'minha task bla bla'}, {id: 2, title: 'Minha task 2', description: 'minha task bla bla'},{id: 3, title: 'Minha task 3', description: 'minha task bla bla'}, {id: 4, title: 'Minha task 4', description: 'minha task bla bla'}]
         },
         {
             id: 2,
             title: "Coluna 2",
-            tasks: [{id: 5, title: 'Minha task 5', descripition: 'minha task bla bla'}, {id: 6, title: 'Minha task 6', descripition: 'minha task bla bla'}, {id: 7, title: 'Minha task 7', descripition: 'minha task bla bla'}]
+            tasks: [{id: 5, title: 'Minha task 5', description: 'minha task bla bla'}, {id: 6, title: 'Minha task 6', description: 'minha task bla bla'}, {id: 7, title: 'Minha task 7', description: 'minha task bla bla'}]
         }],
     }
 ];
@@ -45,56 +47,6 @@ butCreateBoard.addEventListener('click', () => {
     boards.push({id, title: 'New Board', columns: [], tasks: []})
     createBoard(id)
 })
-
-// board builder and the functions it depends on ----------------------------------
-function createBoard(id ,name = '', arrayColumn = []){
-    // creating board item and your configuration
-    const board = document.createElement('div')
-    board.dataset.boardId = id
-    board.classList.add('board')
-
-    const columns = document.createElement('div')
-    columns.classList.add('columns')
-    //add all columns 
-    arrayColumn.forEach((column)=>{
-        columns.appendChild(column)
-    })
-    board.appendChild(columns)
-
-    const butCreateColumn = document.createElement('button')
-    butCreateColumn.classList.add('createColumn')
-    butCreateColumn.innerText = '+'
-    butCreateColumn.addEventListener('click', () => {
-        columns.appendChild(createColumn())
-
-    })
-    board.appendChild(butCreateColumn)
-
-    areaBoards.appendChild(board)
-
-    // creating nav item and your configuration
-    const navBoard = document.createElement('li')
-    navBoard.id = `board${id}`
-    
-    const titleBoard = document.createElement('input')
-    titleBoard.classList.add('titleBoard')
-    titleBoard.type= 'text'
-    titleBoard.placeholder= 'New Board'
-    titleBoard.maxLength= 20
-    titleBoard.readOnly= true
-    titleBoard.value= name
-    titleBoard.addEventListener('dblclick', ()=> {titleBoard.readOnly= false})
-    titleBoard.addEventListener('focusout', ()=> {
-        titleBoard.readOnly= true
-        boardRefreshName(id, titleBoard.value)
-    })
-    navBoard.appendChild(titleBoard)
-
-    navBoard.addEventListener('click', () => activeBoard(navBoard, board))
-    navBoards.appendChild(navBoard)
-
-    return navBoard
-}
 
 function activeBoard(navBoard, board){
     deactivateAll()
@@ -130,19 +82,60 @@ function boardIdGenerator(){
     }
 }
 
-function boardRefreshName(id, title){
-    boards.forEach(board => {
-        if(board.id == id){
-            board.title = title
-        }
-    })
-}
+// board builder and the functions it depends on ----------------------------------
+function createBoard(boardData){
+    // creating board item and your configuration
+    const board = document.createElement('div')
+    board.dataset.boardId = boardData.id
+    board.classList.add('board')
 
+    const columns = document.createElement('div')
+    columns.classList.add('columns')
+    //add all columns 
+    boardData.columns.forEach((columnData)=>{
+        columns.appendChild(createColumn(columnData))
+    })
+    board.appendChild(columns)
+
+    const butCreateColumn = document.createElement('button')
+    butCreateColumn.classList.add('createColumn')
+    butCreateColumn.innerText = '+'
+    butCreateColumn.addEventListener('click', () => {
+        columnData = {id: ++boardData.columnsCount, title: '' , tasks: []}
+        boardData.columns.push(columnData)
+        columns.appendChild(createColumn(columnData))
+    })
+    board.appendChild(butCreateColumn)
+
+    areaBoards.appendChild(board)
+
+    // creating nav item and your configuration
+    const navBoard = document.createElement('li')
+    navBoard.id = `board${boardData.id}`
+    
+    const titleBoard = document.createElement('input')
+    titleBoard.classList.add('titleBoard')
+    titleBoard.type= 'text'
+    titleBoard.placeholder= 'New Board'
+    titleBoard.maxLength= 20
+    titleBoard.readOnly= true
+    titleBoard.value= boardData.title
+    titleBoard.addEventListener('dblclick', ()=> {titleBoard.readOnly= false})
+    titleBoard.addEventListener('focusout', ()=> {
+        titleBoard.readOnly= true
+        boardData.title = titleBoard.value
+    })
+    navBoard.appendChild(titleBoard)
+
+    navBoard.addEventListener('click', () => activeBoard(navBoard, board))
+    navBoards.appendChild(navBoard)
+
+    return navBoard
 
 // column builder and the functions it depends on ----------------------------------
-function createColumn(id, name = '', arrayTask = []){
+function createColumn(columnData){
     const column = document.createElement('div')
-    column.dataset.columnId = id
+    column.dataset.columnId = columnData.id
     column.classList.add('column')
 
     const titleColumn = document.createElement('input')
@@ -151,109 +144,107 @@ function createColumn(id, name = '', arrayTask = []){
     titleColumn.placeholder= 'New Board'
     titleColumn.maxLength= 30
     titleColumn.readOnly= true
-    titleColumn.value= name
+    titleColumn.value= columnData.title
     titleColumn.addEventListener('dblclick', ()=> {titleColumn.readOnly= false})
-    titleColumn.addEventListener('focusout', ()=> {titleColumn.readOnly= true})
+    titleColumn.addEventListener('focusout', ()=> {
+        titleColumn.readOnly= true
+        columnData.title = titleColumn.value
+    })
     column.appendChild(titleColumn)
 
     const listTask = document.createElement('listTask')
     listTask.classList.add('listTask')
     //add all columns 
-    arrayTask.forEach((task)=>{
-        listTask.appendChild(task)
+    columnData.tasks.forEach((task)=>{
+        listTask.appendChild(createTask(task))
     })
     column.appendChild(listTask)
 
     const butCreateTask = document.createElement('button')
     butCreateTask.classList.add('createTask')
     butCreateTask.innerText = '+ Add Task'
-    butCreateTask.addEventListener('click', ()=> openModal(listTask))
+    butCreateTask.addEventListener('click',()=> {
+        const taskData = {id: ++boardData.taskCount, title: '', description: ''}
+        const task = listTask.appendChild(createTask(taskData))
+        openModal(task)
+    }
+    )
     column.appendChild(butCreateTask)
 
     return column
 }
 
-
-
 // Task builder and the functions it depends on ----------------------------------
-function createTask(id, name, arrayTag = []){
+function createTask(taskData){
     const task = document.createElement('li')
-    task.dataset.taskId = id
+    task.dataset.taskId = taskData.id
     task.classList.add('task')
+    task.dataset.description = taskData.description
     task.draggable = true
+
 
     const identificationTask = document.createElement('p')
     identificationTask.classList.add('identificationTask')
-    identificationTask.innerText = `#${id}`
+    identificationTask.innerText = `#${taskData.id}`
     task.appendChild(identificationTask)
 
     const titleTask = document.createElement('p')
     titleTask.classList.add('titleTask')
-    titleTask.innerText = name
-    titleTask.addEventListener('click',()=> openModal(undefined, task))
+    titleTask.innerText = taskData.title
+    titleTask.addEventListener('click',()=>{
+        openModal(task, task)
+    })
     task.appendChild(titleTask)
 
     const tagsTask = document.createElement('div')
     tagsTask.classList.add('tagsTask')
     //add all tag
-    arrayTag.forEach((tag) => {
-        tagsTask.appendChild(tag)
-    })
+    // arrayTag.forEach((tag) => {
+    //     tagsTask.appendChild(tag)
+    // })
     task.appendChild(tagsTask)
 
     return task
 }
 
+}
 
-function openModal(listTask, task = undefined){
+function openModal(task){
     taskModal.classList.add('active')
+    const title = taskModal.querySelector('.title')
+    title.value = task.querySelector('.titleTask').innerText
+    const description = taskModal.querySelector('.description')
+    description.value = task.dataset.description
     const cancel = taskModal.querySelector('.cancel')
-    cancel.addEventListener('click', ()=> taskModal.classList.remove('active'))
+    cancel.addEventListener('click', funCancel)
+
     const confirm = taskModal.querySelector('.confirm')
+    confirm.addEventListener('click', funconfirm)
 
-    if(!task){
-    confirm.addEventListener('click', function modalCreateTask(){
-        const title = taskModal.querySelector('.title')
-        listTask.appendChild(createTask(1, title.value))
-        title.value = ''     
+    function funCancel(){
         taskModal.classList.remove('active')
-        confirm.removeEventListener('click', modalCreateTask)
-    })
-    }else{
-        const title = taskModal.querySelector('.title')
-        const titleTask = task.querySelector('.titleTask')
-        title.value = titleTask.innerText
-
-    confirm.addEventListener('click', function modalRefreshTask(){
-        titleTask.innerText = title.value
-        title.value = ''     
+        cancel.removeEventListener('click', funCancel)
+        confirm.removeEventListener('click', funconfirm)
+    }
+    function funconfirm(){
+        
         taskModal.classList.remove('active')
-        confirm.removeEventListener('click', modalRefreshTask)
-    })
+        taskModal.classList.remove('active')
+        task.querySelector('.titleTask').innerText = title.value
+        task.dataset.description = description.value
+        cancel.removeEventListener('click', funCancel)
+        confirm.removeEventListener('click', funconfirm)
     }
 
 }
 
-
 // initializing dashboard
-const copyBoards = [...boards]
-copyBoards.forEach((board, index) => {
-    const navBoard = boardBuilder(board)
+boards.forEach((board, index) => {
+    const navBoard = createBoard(board)
     if(index === 0){
         const click = new Event('click')
         navBoard.dispatchEvent(click)
     }
 })
 
-function boardBuilder(board){
-    const columns = board.columns
 
-    columns.forEach((column,index,array) => {
-        const tasks = column.tasks
-        tasks.forEach((task, index, array)=>{
-            array[index] = createTask(task.id, task.title)
-        })
-        array[index] = createColumn(column.id, column.title, tasks)
-    })
-    return createBoard(board.id,board.title, columns)
-}
